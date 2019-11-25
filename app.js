17da06b9e30a5a5ee3c357b3bff58760171e42e2
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-11-25 15:14:02 +0100

    Validation for zip code with reg exp

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ function validateName(){
 }
 
 function validateZip(){
-    
+    const zip = document.getElementById('zip');
+    const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+    if(!re.test(zip.value)){
+        zip.classList.add('is-invalid');
+    }else {
+        zip.classList.remove('is-invalid');
+    }
 }
 
 function validateEmail(){
